23e5292059acf3c9be541f376f15607d8682df97
LachlanRMIT <30454053+LachlanRMIT@users.noreply.github.com>
2017-09-26 11:39:49 +1000

    String_api.xml

--- app/src/main/java/com/lachlannewman/stormy/CurrentWeather.java
+++ app/src/main/java/com/lachlannewman/stormy/CurrentWeather.java
@@ -5,4 +5,58 @@ package com.lachlannewman.stormy;
  */
 
 public class CurrentWeather {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipitation;
+    private String Summary;
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipitation() {
+        return mPrecipitation;
+    }
+
+    public void setPrecipitation(double precipitation) {
+        mPrecipitation = precipitation;
+    }
+
+    public String getSummary() {
+        return Summary;
+    }
+
+    public void setSummary(String summary) {
+        Summary = summary;
+    }
 }
