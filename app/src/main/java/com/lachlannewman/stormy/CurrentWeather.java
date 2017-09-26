20c8ba729233846d9d3cd2cb4c55579f0c903fac
LachlanRMIT <30454053+LachlanRMIT@users.noreply.github.com>
2017-09-26 12:21:55 +1000

    iconid

--- app/src/main/java/com/lachlannewman/stormy/CurrentWeather.java
+++ app/src/main/java/com/lachlannewman/stormy/CurrentWeather.java
@@ -1,62 +1,121 @@
 package com.lachlannewman.stormy;
 
+import android.graphics.drawable.Icon;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by Lachlan Newman on 26/09/2017.
  */
 
 public class CurrentWeather {
-    private String mIcon;
-    private long mTime;
-    private double mTemperature;
-    private double mHumidity;
-    private double mPrecipitation;
-    private String Summary;
+    private String icon;
+    private long time; //seconds
+    private double temperature;
+    private double humidity;
+    private double precipitation;
+    private String summary;
+    private String timeZone;
 
     public String getIcon() {
-        return mIcon;
+        return icon;
     }
 
     public void setIcon(String icon) {
-        mIcon = icon;
+        this.icon = icon;
     }
 
     public long getTime() {
-        return mTime;
+        return time;
     }
 
     public void setTime(long time) {
-        mTime = time;
+        this.time = time;
     }
 
     public double getTemperature() {
-        return mTemperature;
+        return temperature;
     }
 
     public void setTemperature(double temperature) {
-        mTemperature = temperature;
+        this.temperature = temperature;
     }
 
     public double getHumidity() {
-        return mHumidity;
+        return humidity;
     }
 
     public void setHumidity(double humidity) {
-        mHumidity = humidity;
+        this.humidity = humidity;
     }
 
     public double getPrecipitation() {
-        return mPrecipitation;
+        return precipitation;
     }
 
     public void setPrecipitation(double precipitation) {
-        mPrecipitation = precipitation;
+        this.precipitation = precipitation;
     }
 
     public String getSummary() {
-        return Summary;
+        return summary;
     }
 
     public void setSummary(String summary) {
-        Summary = summary;
+        this.summary = summary;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public int getIconId(){
+        // clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+        int iconId = R.drawable.clear_day;
+        if (icon.equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        }
+        else if (icon.equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        }
+        else if (icon.equals("rain")) {
+            iconId = R.drawable.rain;
+        }
+        else if (icon.equals("snow")) {
+            iconId = R.drawable.snow;
+        }
+        else if (icon.equals("sleet")) {
+            iconId = R.drawable.sleet;
+        }
+        else if (icon.equals("wind")) {
+            iconId = R.drawable.wind;
+        }
+        else if (icon.equals("fog")) {
+            iconId = R.drawable.fog;
+        }
+        else if (icon.equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        }
+        else if (icon.equals("partly-cloudy-day")) {
+            iconId = R.drawable.partly_cloudy;
+        }
+        else if (icon.equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+
+        return iconId;
+    }
+
+    public String getFormattedTime(){
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(timeZone));
+        Date dateTime = new Date(time * 1000); //convert to milliseconds
+        return formatter.format(dateTime);
     }
 }
